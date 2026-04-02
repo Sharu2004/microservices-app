18b878f0cafce3dfb3524f65cbed7e37108268bc
Sharu2004 <sharukeshava9842@gmail.com>
2026-04-02 18:22:37 +0530

    test ci cd

--- service-1/app.js
+++ service-1/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Service 1 is ok");
+  res.send("CI/CD SUCCESS");
 });
 
 app.listen(3000, () => {
