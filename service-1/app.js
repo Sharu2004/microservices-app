cff9fc3ba5dfd3e5dd33c2c684cf5671fb67e930
Sharu2004 <sharukeshava9842@gmail.com>
2026-04-02 18:15:44 +0530

    update service1

--- service-1/app.js
+++ service-1/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Service 1 update");
+  res.send("Service 1 is ok");
 });
 
 app.listen(3000, () => {
